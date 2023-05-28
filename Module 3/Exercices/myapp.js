9b39882b009c86e82912d9e58ff8353d0fed7bfe
Christian Bauer <52620869+TestBauer@users.noreply.github.com>
2023-05-28 19:50:46 +0200

    module3

---
+++ Module 3/Exercices/myapp.js
@@ -0,0 +1,16 @@
+// this script used to check if the number is even or odd
+
+var num = 6;
+var result = (num % 2  != 0) ? "odd" : "even";
+
+
+if (num === "6"){
+    console.log("test")
+}
+
+
+console.error();
+
+// display the result
+
+console.log(`Number is ${result}.`);
